d40e6a177172910053e93319da7559c7d32fa04c
Talha <Talha@TLHCL7>
2016-11-18 18:11:46 +0300

    Add project files.

---
+++ sadecefikirler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sadecefikirler.MvcApplication" Language="C#" %>
---
+++ sadecefikirler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sadecefikirler.Startup))]
+namespace sadecefikirler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
