220f9daacc58524dc3f5dfdd9bcfa3db2613fdde
Talha <Talha@TLHCL7>
2016-11-19 14:14:38 +0300

    Veri tabanı oluşturduk postları çekip ana sayfaya bastık

--- sadecefikirler/Controllers/PostController.cs
+++ sadecefikirler/Controllers/PostController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using sadecefikirler.Models;
 namespace sadecefikirler.Controllers
 {
-    public class denemeController : Controller
+    public class PostController : Controller
     {
-        // GET: deneme
+        // GET: Post
         public ActionResult Index()
         {
-            return View();
+            var db = new sadecefikirlerEntities();
+            
+            return View(db.Posts.ToList());
         }
     }
 }
