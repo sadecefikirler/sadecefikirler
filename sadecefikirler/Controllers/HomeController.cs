59f062e0dfa2a7a107667e96a59c216b05fbbbf0
emirsozen <emirsozen817@gmail.com>
2016-12-16 17:23:15 +0300

    talha deneme

--- sadecefikirler/Controllers/HomeController.cs
+++ sadecefikirler/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using sadecefikirler.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace sadecefikirler.Controllers
 {
     public class HomeController : Controller
     {
+        PostConcrete pc = new PostConcrete();
+
         public ActionResult Index()
         {
             return View();
